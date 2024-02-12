5d3f4c16ae3ffd0375d847d3d6cdf47a86fdccde
khusni mubarok <husnirqw@gmail.com>
2024-02-12 22:04:18 +0700

    aplikasi belanjaan with react javascript

---
+++ src/components/GroceryList.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Item from "./item";
+export default function GroceryList({
+  items,
+  onDeleteItem,
+  onToggleItem,
+  onClearItems,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  switch (sortBy) {
+    case "name":
+      sortedItems = items.slice().sort((a, b) => a.name.localeCompare(b.name));
+      break;
+    case "checked":
+      sortedItems = items.slice().sort((a, b) => a.checked - b.checked);
+      break;
+    default:
+      sortedItems = items;
+      break;
+  }
+
+  return (
+    <>
+      <div className="list">
+        <ul>
+          {sortedItems.map((item) => (
+            <Item
+              item={item}
+              key={item.id}
+              onDeleteItem={onDeleteItem}
+              onToggleItem={onToggleItem}
+            />
+          ))}
+        </ul>
+      </div>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Urutan berdasarkan urutan input</option>
+          <option value="name">Urutan berdasarkan nama barang</option>
+          <option value="checked">Urutan berdasarkan ceklis</option>
+        </select>
+        <button onClick={onClearItems}>Bersihkan daftar</button>
+      </div>
+    </>
+  );
+}
